40bfaca09f3cfc816e67578706070122d0b7551d
JulienLL <gronuxx@gmail.com>
2026-04-01 17:08:07 +0200

    Refactor entrypoint.sh for service setup and permissions
    Updated permissions and configuration for various services.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,23 +6,30 @@ mkdir -p /home/wims/tmp/log
 mkdir -p /home/wims/public_html
 chown -R wims:wims /home/wims/tmp
 chown -R wims:wims /home/wims/log
+chown -R wims:wims /home/wims/public_html
 
-# Copier wims.cgi au bon endroit
-cp /home/wims/wims/public_html/wims /home/wims/public_html/wims.cgi
-chown wims:wims /home/wims/public_html/wims.cgi
-chmod +x /home/wims/public_html/wims.cgi
+# Configurer les droits sensibles
+cd /home/wims/wims
+./bin/setwrapexec
+./bin/setwimsd
+
+# Configurer Octave
+echo "pkg load statistics" >> /etc/octaverc
+
+# Configurer Povray
+echo "read+write* = /home/wims/tmp/sessions" >> /etc/povray/3.7/povray.conf
+
+# Configurer GAP
+echo 'SetUserPreference( "UseColorPrompt", false );' > /usr/lib/gap/gap.ini
 
 # Configurer Apache
-cd /home/wims/wims
 ./bin/apache-config
+a2enmod cgi
 
-# Lancer wimslogd
-su - wims -c "/home/wims/wims/src/Wimslogd/wimslogd &"
-sleep 1
-
-# Lancer wimsd
-su - wims -c "/home/wims/wims/bin/wimsd &"
-sleep 1
+# Lancer wimsd depuis /home/wims
+cd /home/wims
+su - wims -c "cd /home/wims && ./wims/bin/wimsd &"
+sleep 2
 
 # Lancer Apache au premier plan
 exec apache2ctl -D FOREGROUND
