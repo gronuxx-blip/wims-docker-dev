e3628eaf547af84003b5fdb247ea6e3b6acfc0ea
JulienLL <gronuxx@gmail.com>
2026-04-02 13:48:35 +0200

    Comment out MANAGER_SITE configuration logic
    Comment out the conditional block for MANAGER_SITE and set a default value.

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,13 +31,14 @@ if [ ! -f "$config_file" ]; then
     echo "threshold2=$(($(grep -c processor /proc/cpuinfo) * 300))" >> "$config_file"
     chown wims:wims "$config_file"
     chmod go-rwx "$config_file"
-    if [ -n "$MANAGER_SITE" ]; then
-        if [ "$MANAGER_SITE" = "auto" ]; then
-            echo "manager_site=$(route -n | grep "UG" | awk '{print $2}')" >> "$config_file"
-        else
-            echo "manager_site=$MANAGER_SITE" >> "$config_file"
-        fi
-    fi
+    #if [ -n "$MANAGER_SITE" ]; then
+    #    if [ "$MANAGER_SITE" = "auto" ]; then
+    #        echo "manager_site=$(route -n | grep "UG" | awk '{print $2}')" >> "$config_file"
+    #    else
+    #        echo "manager_site=$MANAGER_SITE" >> "$config_file"
+    #    fi
+    #fi
+    echo "manager_site=127.0.0.1" >> "$config_file"
 fi
 
 # ============================================
